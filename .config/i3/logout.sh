6bfbe222ea8f6635e8dc3da3a4afd1a1d223cc6e
John Parks <jrparks9469@gmail.com>
2020-07-18 01:13:04 -0400

    i3 updates

--- .config/i3/logout.sh
+++ .config/i3/logout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #uses dmenu to confirm logout
-x=$(echo -e "Logout\nShutdown\nRestart" | rofi -width -18 -lines 3 -location 1 -yoffset 20 -dmenu -p "Power Options: ")
+x=$(echo -e "Logout\nShutdown\nRestart" | rofi -font "Hack 10" -width -18 -lines 3 -location 1 -yoffset 20 -dmenu -p "Power Options: ")
 case "$x" in
     Shutdown) systemctl poweroff;;
     Restart) systemctl reboot;;
